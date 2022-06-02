b39ccce5c716e99a91caf42c3ae3698b838e21e2
Seif Ibrahim <seifibrahim10@gmail.com>
2022-06-01 19:47:09 -0700

    separate upstream vs downstream data, fix window logic

--- extractor/youtube_stats_extractor.py
+++ extractor/youtube_stats_extractor.py
@@ -50,12 +50,17 @@ def shift_window(window, start_time, data, data_idx):
     :return: the updated data_index or throws exception if reached end
     """
     # move left side
-    while not window or window[0]['timestamp'] < start_time:
-        if not window:
-            window.append(data[data_idx])
+    while window and window[0]['timestamp'] < start_time:
+        window.popleft()
+
+    # find the first element to add
+    if not window:
+        while data[data_idx]['timestamp'] < start_time:
             data_idx += 1
+        if data[data_idx]['timestamp'] < start_time + WINDOW_SIZE:
+            window.append(data[data_idx])
         else:
-            window.popleft()
+            return data_idx
 
     # move right side
     while window[-1]['timestamp'] < start_time + WINDOW_SIZE:
@@ -65,16 +70,81 @@ def shift_window(window, start_time, data, data_idx):
     return data_idx
 
 
+def get_network_summary_stats(packets):
+    return {
+        'bytes_per_packet':
+        array_stats([packet['size'] for packet in packets]),
+        'inter_arrival_time':
+        array_stats([
+            packets[i]['timestamp'] - packets[i - 1]['timestamp']
+            for i in range(1, len(packets))
+        ]),
+    }
+
+
+def get_network_window_stats(packet_window, window_start):
+    packet_count = len(packet_window)
+    byte_count = sum(packet['size'] for packet in packet_window)
+    packet_count = len(packet_window)
+    throughput = byte_count / WINDOW_SIZE
+
+    return {
+        'byte_count': byte_count,
+        'packet_count': packet_count,
+        'throughput': throughput,
+        'idle_time': idle_time(packet_window, window_start),
+    }
+
+
+def get_QoE_window_stats(report, window_start):
+    return {
+        'median_resolution':
+        int(
+            statistics.median(
+                int(re.match(r'\d+x(\d+).*', r['resolution']).group(1))
+                for r in report))
+    }
+
+
+def get_ping_window_stats(ping_window, window_start):
+    return {'mean': statistics.mean(ping['ping'] for ping in ping_window)}
+
+
+def get_window_stats(data, window_start, extract_func):
+    get_window_stats = []
+    data_idx = 0
+    window = deque()
+
+    while True:
+        try:
+            data_idx = shift_window(window, window_start, data, data_idx)
+        except IndexError:
+            break
+
+        if window:
+            get_window_stats.append(extract_func(window, window_start))
+        else:
+            # empty window
+            get_window_stats.append(None)
+
+        window_start += WINDOW_SHIFT
+
+    return get_window_stats
+
+
 def youtube_stats_extractor():
-    # pcap_file = "../youtube_data/dQw4w9WgXcQ_0/169.231.179.199_43828_173.194.166.91_443.pcap"
-    pcap_file = "../youtube_data/dQw4w9WgXcQ_0/169.231.179.199_43830_173.194.166.91_443.pcap"
-    state_file = "../youtube_data/dQw4w9WgXcQ_0/state.txt"
-    report_file = "../youtube_data/dQw4w9WgXcQ_0/report.txt"
-    ping_file = "../youtube_data/dQw4w9WgXcQ_0/ping.txt"
-    meta_file = "../youtube_data/dQw4w9WgXcQ_0/meta.txt"
+    report_dir = "/home/seif/Documents/CS293N/project/pinot_minion_tasks/youtube_data/dQw4w9WgXcQ_0"
+    pcap_file = "169.231.178.179_35416_198.189.66.16_443.pcap"
+    pcap_path = f"{report_dir}/{pcap_file}"
+    state_path = f"{report_dir}/state.json"
+    report_path = f"{report_dir}/report.json"
+    ping_path = f"{report_dir}/ping.txt"
+    meta_path = f"{report_dir}/meta.json"
+
+    src_ip = pcap_file.split('_')[0]
 
     video_start = 0
-    with open(state_file, 'r') as f:
+    with open(state_path, 'r') as f:
         for line in f:
             state = json.loads(line)
             if state['new_state'] == 1:
@@ -82,24 +152,38 @@ def youtube_stats_extractor():
                 break
 
     startup_delay = 0
-    with open(meta_file, 'r') as f:
+    with open(meta_path, 'r') as f:
         startup_delay = json.loads(f.read())['startup_delay']
 
     # read application layer report
     report = None
-    with open(report_file, 'r') as f:
+    with open(report_path, 'r') as f:
         report = [json.loads(line) for line in f]
+    for r in report:
+        r['timestamp'] /= 1000
 
     # read network layer capture (we only use timestamp and packet size)
-    packets = [{
-        'timestamp': float(packet.sniff_timestamp),
-        'size': int(packet.data.len)
-    } for packet in pyshark.FileCapture(pcap_file)]
+    capture = pyshark.FileCapture(pcap_path)
+    upstream = []
+    downstream = []
+    for packet in capture:
+        if packet.ip.src == src_ip:
+            upstream.append({
+                'timestamp': float(packet.sniff_timestamp),
+                'size': int(packet.ip.len)
+            })
+        else:
+            downstream.append({
+                'timestamp': float(packet.sniff_timestamp),
+                'size': int(packet.ip.len)
+            })
+    upstream.sort(key=lambda packet: packet['timestamp'])
+    downstream.sort(key=lambda packet: packet['timestamp'])
 
     # read last mile latency ping
     pings = []
     ping_stats = {}
-    with open(ping_file, 'r') as f:
+    with open(ping_path, 'r') as f:
         for line in f:
             match = re.match(r'\[(.*)\].*time=(.*) ms', line)
             if match:
@@ -117,77 +201,36 @@ def youtube_stats_extractor():
                     'mdev': float(match.group(4)) / 1000,
                 }
 
-    # window pointers
-    window_start = video_start
-    report_idx = 0
-    report_window = deque()
-    packet_idx = 0
-    packet_window = deque()
-    ping_idx = 0
-    ping_window = deque()
-
     # collect window stats
-    window_stats = []
+    upstream_window_stats = get_window_stats(upstream, video_start,
+                                             get_network_window_stats)
+    downstream_window_stats = get_window_stats(downstream, video_start,
+                                               get_network_window_stats)
+    QoE_window_stats = get_window_stats(report, video_start,
+                                        get_QoE_window_stats)
+    ping_window_stats = get_window_stats(pings, video_start,
+                                         get_ping_window_stats)
 
-    while True:
-        try:
-            shift_window(report_window, window_start, report, report_idx)
-        except IndexError:
-            print('Ran out of stats for nerds')
-            break
-        try:
-            shift_window(packet_window, window_start, packets, packet_idx)
-        except IndexError:
-            # we expect to run out of packet caps first since we typically
-            # finish downloading the video before we finish watching it
-            print('Ran out of packet data')
-            break
-        try:
-            shift_window(ping_window, window_start, pings, ping_idx)
-        except IndexError:
-            print('Ran out of ping data')
-            break
-
-        byte_count = sum(packet['size'] for packet in packet_window)
-        packet_count = len(packet_window)
-        throughput = byte_count / WINDOW_SIZE
-        # example resolution str "640x360@25 / 854x480@25"
-        median_resolution = int(
-            statistics.median(
-                int(re.match(r'\d+x(\d+).*', r['resolution']).group(1))
-                for r in report))
-        mean_ping = statistics.mean(ping['ping'] for ping in ping_window)
-
-        window_stats.append({
-            'network': {
-                'byte_count': byte_count,
-                'packet_count': packet_count,
-                'throughput': throughput,
-                'idle_time': idle_time(packet_window, window_start),
-            },
-            'QoE': {
-                'median_resolution': median_resolution,
-            },
-            'ping': {
-                'mean': mean_ping,
-            },
-        })
-
-        window_start += WINDOW_SHIFT
+    window_stats = [{
+        'network': {
+            'upstream': upstream,
+            'downstream': downstream,
+        },
+        'QoE': QoE,
+        'ping': ping,
+    }
+                    for upstream, downstream, QoE, ping in zip(
+                        upstream_window_stats, downstream_window_stats,
+                        QoE_window_stats, ping_window_stats)]
 
     # collect summary statistics
     summary_stats = {
-        'ping': ping_stats,
-        'startup_delay': startup_delay,
         'network': {
-            'bytes_per_packet':
-            array_stats([packet['size'] for packet in packets]),
-            'inter_arrival_time':
-            array_stats([
-                packets[i]['timestamp'] - packets[i - 1]['timestamp']
-                for i in range(1, len(packets))
-            ]),
+            'upstream': get_network_summary_stats(upstream),
+            'downstream': get_network_summary_stats(downstream),
         },
+        'ping': ping_stats,
+        'startup_delay': startup_delay / 1000,
     }
 
     print(window_stats)
