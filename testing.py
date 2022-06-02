627031be2b30ff6c9ea1537ffdd356f339f1d9da
jaber-the-great <78180441+jaber-the-great@users.noreply.github.com>
2022-06-02 10:07:59 -0700

    Create testing.py
    test the arg length

---
+++ testing.py
@@ -0,0 +1,14 @@
+import sys
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Usage: python3 example.py <video> <duration> <data_dump>")
+        print("Executing with default values: python3 example.py"
+              " https://www.youtube.com/watch?v=dQw4w9WgXcQ 10 youtube_data")
+        video = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        duration = 10
+        data_dump = "youtube_data"
+    else:
+        print("length is 4 and ok")
+        video = sys.argv[1]
+        duration = int(sys.argv[2])
+        data_dump = sys.argv[3]
